ae05e81a93c451a047e5d1f0387d7c76666025bf
Majeedl12 <132606472+Majeedl12@users.noreply.github.com>
2026-06-02 01:47:05 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
     const { hwid } = req.query;
     if (!hwid) return res.status(400).send('Missing hwid');
 
