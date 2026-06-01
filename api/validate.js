3939448eba43a29c1f5a12b1ca5553aabaa8e3c1
Majeedl12 <132606472+Majeedl12@users.noreply.github.com>
2026-06-02 01:03:47 +0300

    Update validate.js

--- api/validate.js
+++ api/validate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const { key, hwid } = req.query;
-    if (!key || !hwid) return res.status(200).send('false');
+    if (!key) return res.status(200).send('false');
 
     const url = 'https://fluent-stag-141278.upstash.io';
     const token = 'gQAAAAAAAifeAAIgcDE5YmIwNzM0ZDNhZjA0N2RhODllY2UwY2YxNDhhZjZmMg';
@@ -11,14 +11,20 @@ export default async function handler(req, res) {
         });
         if (resp.ok) {
             const data = await resp.json();
-            const parsed = JSON.parse(data.result);
-            if (parsed.hwid === hwid && Date.now() < parsed.expiresAt) {
-                return res.status(200).send('true');
-            } else {
-                 
-                await fetch(`${url}/del/${key}`, {
-                    headers: { 'Authorization': `Bearer ${token}` }
-                });
+            if (data.result) {
+                const parsed = JSON.parse(data.result);
+                if (hwid) {
+                    if (parsed.hwid === hwid && Date.now() < parsed.expiresAt) {
+                        return res.status(200).send('true');
+                    }
+                } else {
+                    if (Date.now() < parsed.expiresAt) {
+                        return res.status(200).send('true');
+                    }
+                }
+                if (Date.now() >= parsed.expiresAt) {
+                    await fetch(`${url}/del/${key}`, { headers: { 'Authorization': `Bearer ${token}` } });
+                }
             }
         }
     } catch (e) {}
